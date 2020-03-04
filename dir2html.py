699e95ca1f966cdeac082952cd8ecca37eef89e6
kongkong <huyanghun@hotmail.com>
2020-03-04 14:49:47 +0800

    update

--- dir2html.py
+++ dir2html.py
@@ -5,34 +5,41 @@
 import sys
 import os
 
-def recurrence(path,filenames):
-    if len(os.listdir(path))>20:
+def read_dir(root,path,filenames):
+    if len(os.listdir(path))>25:
         file=str(os.listdir(path)[0])+"……等"
         filenames.append('<ul><li >%s</a></li></ul>\n'%file)
     else:
         for file in os.listdir(path):
-            fs = os.path.join(path, file)      
-            root="D:\\APP DATA\\Seafile\\Seafile\\D-PD-19014-10.叶片故障监测系统\\"
+            fs = os.path.join(path, file)
             path_w=str(fs).replace(root, "")
             if os.path.isfile(fs):            
                 filenames.append('<ul><li ><a href="file:%s\\">%s</a></li></ul>\n'%(path_w,file))
             elif os.path.isdir(fs):
                 filenames.append('<ul><li ><a href="file:%s\\">%s</a></li>\n'%(path_w,file))
-                recurrence(fs, filenames)
+                read_dir(root,fs, filenames)
                 filenames.append('</ul>\n')
-if __name__=="__main__":
-    path = 'D:\\APP DATA\\Seafile\\Seafile\\D-PD-19014-10.叶片故障监测系统'
-    filenames = []  # 输出列表
-    start="""<html>
+
+start="""<html>
 <head>
 <title>
 叶片卫士
 </title>
 </head>
 <body>"""
+
+end="""</body>
+</html>
+"""
+
+if __name__=="__main__":
+    path = 'D:\\APP DATA\\Seafile\\Seafile\\D-PD-19014-10.叶片故障监测系统'
+    root=path+'\\'
+    outputfile='目录.html'
+
+    filenames = []  # 输出列表
     filenames.append(start)
-    recurrence(path,filenames)
-    filenames.append("<//body>")
-    filenames.append("<//html>")
-    with open('目录.html', 'w', encoding='utf-8') as f:
+    read_dir(root,path,filenames)
+    filenames.append(end)
+    with open(outputfile, 'w', encoding='utf-8') as f:
         f.writelines(filenames)
